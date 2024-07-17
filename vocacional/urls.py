1b0d47b02c3c59dfb136cb3b5e5a997d99051d89
Gleiser Wesley <38473705+gwpoliveira@users.noreply.github.com>
2024-07-17 08:41:58 -0300

    foi

--- vocacional/urls.py
+++ vocacional/urls.py
@@ -1,9 +1,13 @@
 from django.contrib import admin
 from django.urls import path, include
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name='home'),  # Adiciona a URL para a página inicial
     path('quiz/', include('quiz.urls')),
 ]
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
